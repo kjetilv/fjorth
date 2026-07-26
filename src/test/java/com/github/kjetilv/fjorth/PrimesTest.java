f67863d73bf8abaec31a0e942f5256c151bd3f19
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2026-07-26 14:10:36 +0200

    Imports

--- src/test/java/com/github/kjetilv/fjorth/PrimesTest.java
+++ src/test/java/com/github/kjetilv/fjorth/PrimesTest.java
@@ -1,7 +1,6 @@
 package com.github.kjetilv.fjorth;
 
 import module java.base;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
